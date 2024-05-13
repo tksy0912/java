4e59d8c1d3a804cac688b0aa41134fc7cba9d16f
EC2 Default User <takizawasayuki0912@gmail.com>
2024-05-13 13:20:34 +0000

    [Finish] 1-6

--- Main.java
+++ Main.java
@@ -4,5 +4,15 @@ class Main {
     // "文字列"
     // ()の中を出力せよ、という命令
     // javaは文末に;が必須
+
+    // int型の変数
+    int number;
+    number = 3;
+    System.out.println(number);
+
+    // String型の変数
+    String name;
+    name = "Wanko";
+    System.out.println(name);
   }
 }
