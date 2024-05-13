21cd92eb2c3eebb589c90c842b2b541baacd9bc4
EC2 Default User <takizawasayuki0912@gmail.com>
2024-05-13 13:30:02 +0000

    [Finish] 1-8

--- Main.java
+++ Main.java
@@ -5,6 +5,7 @@ class Main {
     // ()の中を出力せよ、という命令
     // javaは文末に;が必須
 
+
     // int型の変数
     int number;
     number = 3;
@@ -14,5 +15,29 @@ class Main {
     String name;
     name = "Wanko";
     System.out.println(name);
+
+
+    // 変数の初期化
+    int number1 = 3;
+    int number2 = 7;
+    System.out.println(number1 + number2);
+    // 「10」が出力
+
+    String text = "プログラミングを勉強しよう";
+    System.out.println("Progateで" + text);
+    // 「Progateでプログラミングを勉強しよう」が出力
+
+
+    // 変数の上書き
+    int number = 11;
+    String text = "Ruby";
+    System.out.println(number);
+    System.out.println(text);
+
+    number = 9;
+    System.out.println(number);
+
+    text = "Java";
+    System.out.println(text);
   }
 }
