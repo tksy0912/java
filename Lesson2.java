1fee32abacb1e8b0294042338c7ad78cfd67fd44
EC2 Default User <takizawasayuki0912@gmail.com>
2024-05-15 13:22:31 +0000

    [Finish] 2-4

--- Lesson2.java
+++ Lesson2.java
@@ -36,5 +36,27 @@ class Main {
     // 「8 < 5」でない、の結果を出力
     System.out.println(!(8 < 5));
     
+    
+    // 条件分岐（if文）
+    // 条件式に直接trueを入れる
+    if (true) {
+      System.out.println("条件式がtrueのため、出力されます");
+    }
+    
+    // 条件式に直接falseを入れる
+    if (false) {
+      System.out.println("条件式がfalseのため、出力されません");
+    }
+    
+    int x = 5;
+    // if文を用いて、変数xが2より大きいとき「xは2より大きい」と出力
+    if (x > 2) {
+      System.out.println("xは2より大きい");
+    }
+    // if文を用いて、変数xが8より大きいとき「xは8より大きい」と出力
+    if (x > 8) {
+      System.out.println("xは8より大きい");
+    }
+    
   }
 }
