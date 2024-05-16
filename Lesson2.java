ddf58d071f2298e028e2885392278b43237373b1
EC2 Default User <takizawasayuki0912@gmail.com>
2024-05-16 07:15:46 +0000

    [Finish] 2-6

--- Lesson2.java
+++ Lesson2.java
@@ -70,5 +70,16 @@ class Main {
       System.out.println("20以上");
     }
     
+    
+    // switch文
+    int n = 1;
+    switch (n) {
+      case 1 :
+        System.out.println("大吉です");
+        break;
+      case 2 :
+        System.out.println("吉です");
+        break;
+    }
   }
 }
