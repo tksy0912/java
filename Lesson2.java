cf03840751316ead025b31b989b149788ab6e63f
EC2 Default User <takizawasayuki0912@gmail.com>
2024-05-16 07:12:17 +0000

    [Finish] 2-5

--- Lesson2.java
+++ Lesson2.java
@@ -58,5 +58,17 @@ class Main {
       System.out.println("xは8より大きい");
     }
     
+    
+    // else、else if
+    int number = 12;
+    // numberが20より小さいとき、どちらでもないときの条件分岐を追加してください
+    if (number < 10) {
+      System.out.println("10より小さい");
+    } else if (number < 20) {
+      System.out.println("10以上、20より小さい");
+    } else {
+      System.out.println("20以上");
+    }
+    
   }
 }
